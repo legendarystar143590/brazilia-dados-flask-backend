d260799d4f6bad0e3f4c77d91b28e8f5d817c7f9
legendarystar143590 <jacob143590@gmail.com>
2024-10-28 05:22:58 +0200

    updated:shopify integration

--- api/shopify.py
+++ api/shopify.py
@@ -6,7 +6,7 @@ import hmac
 import hashlib
 import base64
 import os
-from models import ShopInfo, ProudctsTable, User, Bot
+from models import ShopInfo, ProudctsTable, User, Bot, RegisteredWebsite
 
 load_dotenv()
 
@@ -28,73 +28,82 @@ def verify_webhook(data: bytes, hmac_header: str) -> bool:
 
 @shopify_blueprint.route('/shopifyinstall', methods=['GET'])
 def install():
-    shop = request.args.get('shop')
-    timestamp = request.args.get('timestamp')
-    hmac_header = request.args.get('hmac')
-    state = request.args.get('state')
-    print("shop", shop)
-    # Verify the webhook using the provided parameters
-    data = f'shop={shop}&timestamp={timestamp}&state={state}'.encode('utf-8')
-    if verify_webhook(data, hmac_header):
-        # Store the shop code and state securely (implement your own storage logic)
-        store_shop_data(shop, state)
-        print("Verification successful")
-        # Redirect to Shopify authorization URL
-        return redirect(f'https://{shop}/admin/oauth/authorize?client_id={SHOPIFY_API_KEY}&scope=read_products&redirect_uri={REDIRECT_URI}&state={state}')
-    
-    return "Verification failed", 403
+    try:
+        shop = request.args.get('shop')
+        timestamp = request.args.get('timestamp')
+        hmac_header = request.args.get('hmac')
+        state = request.args.get('state')
+        print("shop", shop)
+        # Verify the webhook using the provided parameters
+        data = f'shop={shop}&timestamp={timestamp}&state={state}'.encode('utf-8')
+        if verify_webhook(data, hmac_header):
+            # Store the shop code and state securely (implement your own storage logic)
+            store_shop_data(shop, state)
+            print("Verification successful")
+            # Redirect to Shopify authorization URL
+            return redirect(f'https://{shop}/admin/oauth/authorize?client_id={SHOPIFY_API_KEY}&scope=read_products&redirect_uri={REDIRECT_URI}&state={state}')
+        
+        return "Verification failed", 403
+    except Exception as e:
+        print("Error:", e)
+        return "An error occurred", 500
 
 @shopify_blueprint.route('/shopifyauth', methods=['GET'])
 def auth_callback():
-    code = request.args.get('code')
-    shop = request.args.get('shop')
-    state = request.args.get('state')
-    timestamp = request.args.get('timestamp')
-    hmac_header = request.args.get('hmac')
-    print("shop", shop)
-    data = f'shop={shop}&code={code}&state={state}&timestamp={timestamp}'.encode('utf-8')
-    # Verify that state matches what was stored during installation
-    if verify_webhook(data, hmac_header):
-        print("Verification successful")
-        if verify_state(shop, state):
-            print("State verification successful")
-            # Store the hashed code with the shop code securely (implement your own storage logic)
-            access_token = get_access_token(shop, code)
-            store_hashed_code(shop, code, access_token)
+    try:
+        code = request.args.get('code')
+        shop = request.args.get('shop')
+        state = request.args.get('state')
+        timestamp = request.args.get('timestamp')
+        hmac_header = request.args.get('hmac')
+        print("shop", shop)
+        data = f'shop={shop}&code={code}&state={state}&timestamp={timestamp}'.encode('utf-8')
+        # Verify that state matches what was stored during installation
+        if verify_webhook(data, hmac_header):
+            print("Verification successful")
+            if verify_state(shop, state):
+                print("State verification successful")
+                # Store the hashed code with the shop code securely (implement your own storage logic)
+                access_token = get_access_token(shop, code)
+                store_hashed_code(shop, code, access_token)
 
-            products = get_shopify_products(shop, access_token)
-            current_shop = ShopInfo.query.filter_by(shop = shop).first()            
-            for product in products:
-              intert_product_data(product, current_shop.id)
+                products = get_shopify_products(shop, access_token)
+                current_shop = ShopInfo.query.filter_by(shop = shop).first()            
+                for product in products:
+                    insert_product_data(product, current_shop.id)
 
-            return {'status':"success",'message':f'Authorization successful'}, 200
-    
-    return "State verification failed", 403
+                activebots = get_active_chatbots(shop)[0].get_json()
+
+                return {'status':"success",'message':f'Authorization successful', 'activebots': activebots}, 200
+        
+        return "State verification failed", 403
+    except Exception as e:
+        print("Error:", e)
+        return "Error occurred", 500
 
 # @shopify_blueprint.route('/active_chatbots', methods=['GET'])
-# @verify_webhook
-# def get_active_chatbots():
-#     shop = request.args.get('shop')
-#     shop_info = ShopInfo.get_by_shop(shop)
-    
-#     if not shop_info:
-#         return jsonify({"error": "Please obtain a valid eCommerce license in Aiana to let your website visitors answer questions about your products."}), 403
+def get_active_chatbots(shop:str):
+    try:
+        # shop = request.args.get('shop')
+        shop_info = ShopInfo.get_by_shop(shop)
+        
+        if not shop_info:
+            return jsonify({"error": "Please obtain a valid eCommerce license in Aiana to let your website visitors answer questions about your products."}), 403
 
-#     user = User.get_by_userID(shop_info.user_id)
-#     if not user:
-#         return jsonify({"error": "User not found"}), 404
+        registered_website = RegisteredWebsite.query.filter_by(domain=shop).first()
+        if not registered_website:
+            return jsonify({"error": "Website not registered"}), 404
+        bot = Bot.query.filter_by(index = registered_website.bot_id).first()
+        response = []
+        response.append({
+            "name": bot.name,
+            "chatbotid": registered_website.bot_id,
+            "userid": registered_website.user_id,
+        })
 
-#     active_bots = Bot.query.filter_by(user_id=user.id, active=1).all()
-    
-#     response = []
-#     for bot in active_bots:
-#         response.append({
-#             "name": bot.name,
-#             "chatbotid": bot.index,
-#             "userid": user.index
-#         })
-
-#     return jsonify(response), 200
+        return jsonify(response), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 def store_shop_data(shop: str, state: str):
     """
@@ -162,25 +171,34 @@ def get_shopify_products(shop: str, access_token: str) -> list:
     """
     # Implement your logic to retrieve products using the provided access token
     # This is a placeholder and should be replaced with actual API calls
-    new_url = f'https://{shop}/admin/api/2023-04/products.json'
-    headers = {
-        'Content-Type': 'application/json',
-        'X-Shopify-Access-Token': access_token
-    }
-    response = requests.get(new_url, headers=headers)
-    if response.status_code == 200:
-        return response.json()['products']
-    else:
+    try:
+        new_url = f'https://{shop}/admin/api/2023-04/products.json'
+        headers = {
+            'Content-Type': 'application/json',
+            'X-Shopify-Access-Token': access_token
+        }
+        response = requests.get(new_url, headers=headers)
+        if response.status_code == 200:
+            return response.json()['products']
+        else:
+            return []
+    except Exception as e:
+        print(e)
         return []
     
-def intert_product_data(product: dict, shop_id: int):
-        """
-        Insert product data into the database.
-        """
-        # Implement your logic to insert product data into the database
-        # This is a placeholder and should be replaced with actual database operations
+def insert_product_data(product: dict, shop_id: int):
+    """
+    Insert product data into the database.
+    """
+    # Implement your logic to insert product data into the database
+    # This is a placeholder and should be replaced with actual database operations
+    try:
         new_product = ProudctsTable(shop_id=shop_id, product_id=product['id'], product_type=product['type'], product_title=product['title'], product_price=product['price'])
         print(new_product)
+        return jsonify({'status':'success'}), 200
+    except Exception as e:
+        print(e)
+        return jsonify({'status':'failed'}), 400
 
 def sync_products():
     """
@@ -188,14 +206,19 @@ def sync_products():
     """
     # Implement your logic to synchronize products from Shopify to the database
     # This is a placeholder and should be replaced with actual API calls
-    with current_app.app_context():
-        ProudctsTable.clear_all_products()
-        shops = ShopInfo.query.all()
-        print("sync_products()", shops)
-        for shop in shops:
-            access_token = shop.access_token
-            if access_token == '':
-                continue
-            products = get_shopify_products(shop.shop, access_token)
-            for product in products:
-                intert_product_data(product, shop.id)
+    try:
+        with current_app.app_context():
+            ProudctsTable.clear_all_products()
+            shops = ShopInfo.query.all()
+            print("sync_products()", shops)
+            for shop in shops:
+                access_token = shop.access_token
+                if access_token == '':
+                    continue
+                products = get_shopify_products(shop.shop, access_token)
+                for product in products:
+                    insert_product_data(product, shop.id)
+        return jsonify({'message': 'Products synchronized successfully'}), 200
+    except Exception as e:
+        print(e)
+        return jsonify({'error': 'Failed to synchronize products'}), 500
